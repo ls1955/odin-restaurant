ff86d7f335e7216ef7b18f394ad56b6502c58f55
Cheong <xxlsxx1955@gmail.com>
2023-08-13 21:23:13 +0800

    Include contactBtn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { setContactContent } from "./contact.js";
 import { setHomepageContent } from "./homepage.js";
 import { setMenuContent } from "./menu.js";
 
@@ -5,15 +6,19 @@ function setNav() {
     const nav = document.createElement("nav");
     const homepageBtn = document.createElement("button");
     const menuBtn = document.createElement("button");
+    const contactBtn = document.createElement("button");
 
     homepageBtn.textContent = "Homepage";
     menuBtn.textContent = "Menu";
+    contactBtn.textContent = "Contact";
 
     homepageBtn.addEventListener("click", () => setHomepageContent());
     menuBtn.addEventListener("click", () => setMenuContent());
+    contactBtn.addEventListener("click", () => setContactContent());
 
     nav.appendChild(homepageBtn);
     nav.appendChild(menuBtn);
+    nav.appendChild(contactBtn);
 
     const mainContent = document.querySelector("main");
     document.body.insertBefore(nav, mainContent);
